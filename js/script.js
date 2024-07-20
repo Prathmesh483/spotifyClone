a79c4a2fb34225e0abaf1c05acbc574e52936d34
Prathmesh483 <137476274+Prathmesh483@users.noreply.github.com>
2024-07-20 14:46:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`https://sptfycloneweb.netlify.app/${folder}`);
+  let a = await fetch(`https://sptfycloneweb.netlify.app/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -89,7 +89,7 @@ const playMusic = (track, pause = false) => {
 async function displayAlbums() {
   console.log("displaying albums");
   try {
-    let a = await fetch(`https://sptfycloneweb.netlify.app/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -101,7 +101,7 @@ async function displayAlbums() {
       if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
         let folder = e.href.split("/").slice(-1)[0];
         // Get the metadata of the folder
-        let a = await fetch(`https://sptfycloneweb.netlify.app/${folder}/info.json`);
+        let a = await fetch(`/songs/${folder}/info.json`);
         let response = await a.json();
         cardContainer.innerHTML += `
           <div data-folder="${folder}" class="card">
