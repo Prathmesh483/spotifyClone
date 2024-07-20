452bd7810e2e3ae0b0349b94a6b6539d72a9071e
Prathmesh483 <137476274+Prathmesh483@users.noreply.github.com>
2024-07-20 11:18:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ async function getSongs(folder) {
         songUL.innerHTML += `<li><img class="invert" width="34" src="img/music.svg" alt="">
             <div class="info">
                 <div>${song.replaceAll("%20", " ")}</div>
-                <div>Harry</div>
+                <div>Singer</div>
             </div>
             <div class="playnow">
                 <span>Play Now</span>
